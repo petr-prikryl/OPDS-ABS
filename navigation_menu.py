f86d3b7876f1341b9d9bfc53c1b1784a43d59d91
Chris Helming <chris.helming@gmail.com>
2025-03-29 21:17:40 -0400

    split into modules and add nav menu

--- navigation_menu.py
+++ navigation_menu.py
@@ -3,34 +3,29 @@ This module provides navigation and  base64-encoded PNGs
 """
 navigation = [
         {
-            "name":   "Items",
-            "desc":   "View all items in your library",
-            "path":   "items",
-            "params": "sort=media.metadata.title"
+            "name": "Items",
+            "desc": "View all items in your library",
+            "path": "items",
         },
         {
-            "name":   "Recent",
-            "desc":   "View recently added items",
-            "path":   "items",
-            "params": "sort=addedAt&desc=1"
+            "name": "Recent",
+            "desc": "View recently added items",
+            "path": "items?sort=addedAt",
         },
         {
-            "name":   "Series",
-            "desc":   "Browse by series",
-            "path":   "series",
-            "params": ""
+            "name": "Series",
+            "desc": "Browse by series",
+            "path": "series",
         },
         {
-            "name":   "Collections",
-            "desc":   "Browse by collection",
-            "path":   "collections",
-            "params": ""
+            "name": "Collections",
+            "desc": "Browse by collection",
+            "path": "collections",
         },
         {
-            "name":   "Authors",
-            "desc":   "Browse by author",
-            "path":   "authors",
-            "params": ""
+            "name": "Authors",
+            "desc": "Browse by author",
+            "path": "authors",
         }
 ]
 
