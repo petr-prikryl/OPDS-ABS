f4f2ef1cf1fbd73641388a0eb39fe6d0a26225bf
Chris Helming <chris.helming@gmail.com>
2025-03-31 20:53:00 -0400

    add series sorting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """Routes for the OPDS feed"""
+from urllib.parse import unquote
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -26,6 +27,12 @@ async def opds_nav(username: str, library_id: str):
     return await opds_feed.generate_nav_feed(username, library_id)
 
 @app.get("/opds/{username}/library/{library_id}/items")
-async def opds_library(username: str, library_id: str, sort: str = None):
+async def opds_library(username: str, library_id: str, r: Request):
     """List of books in a specific library for a specific user"""
-    return await opds_feed.generate_library_feed(username, library_id, sort)
+    params = dict(r.query_params)
+    return await opds_feed.generate_library_items_feed(username, library_id, params)
+
+@app.get("/opds/{username}/library/{library_id}/series")
+async def opds_authors(username: str, library_id: str):
+    """List of series in a specific library for a specific user"""
+    return await opds_feed.generate_series_feed(username, library_id)
--- navigation_menu.py
+++ navigation_menu.py
@@ -3,29 +3,34 @@ This module provides navigation and  base64-encoded PNGs
 """
 navigation = [
         {
-            "name": "Items",
-            "desc": "View all items in your library",
-            "path": "items",
+            "name":   "Items",
+            "desc":   "View all items in your library",
+            "path":   "items",
+            "params": "sort=media.metadata.title"
         },
         {
-            "name": "Recent",
-            "desc": "View recently added items",
-            "path": "items?sort=addedAt",
+            "name":   "Recent",
+            "desc":   "View recently added items",
+            "path":   "items",
+            "params": "sort=addedAt&desc=1"
         },
         {
-            "name": "Series",
-            "desc": "Browse by series",
-            "path": "series",
+            "name":   "Series",
+            "desc":   "Browse by series",
+            "path":   "series",
+            "params": ""
         },
         {
-            "name": "Collections",
-            "desc": "Browse by collection",
-            "path": "collections",
+            "name":   "Collections",
+            "desc":   "Browse by collection",
+            "path":   "collections",
+            "params": ""
         },
         {
-            "name": "Authors",
-            "desc": "Browse by author",
-            "path": "authors",
+            "name":   "Authors",
+            "desc":   "Browse by author",
+            "path":   "authors",
+            "params": ""
         }
 ]
 
